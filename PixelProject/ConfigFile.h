47ec194d5e7e9aae8e7a0b8ac40f832b71a5312d
James Kellie <Jpk_GameID@hotmail.com>
2020-12-29 23:01:55 +1100

    Create ConfigFile.h
    A serializable class for application configs.
    So far only has Screen Width/Height & target fps

---
+++ PixelProject/ConfigFile.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ISerializable.h"
+
+struct ConfigFile final : public ISerializable
+{
+		int screen_x = 1280;
+		int screen_y = 720;
+
+		float target_frames_per_second = 60.0;
+
+		std::string FilePath() override { return std::string("config.json"); }
+
+		// Inherited via ISerializable
+		virtual void Save(cereal::JSONOutputArchive out_archive) override {
+				out_archive(CEREAL_NVP(screen_x));
+				out_archive(CEREAL_NVP(screen_y));
+				out_archive(CEREAL_NVP(target_frames_per_second));
+		}
+		virtual void Load(cereal::JSONInputArchive in_archive) override {
+				in_archive(screen_x);
+				in_archive(screen_y);
+				in_archive(target_frames_per_second);
+		}
+}
