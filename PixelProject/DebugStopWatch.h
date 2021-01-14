ab16bd0ef9dc7a4e5d327157d416aaeaef2bf1d5
James Kellie <Jpk_GameID@hotmail.com>
2021-01-15 01:02:06 +1100

    Update DebugStopWatch.h
    Fixed tabbing

--- PixelProject/DebugStopWatch.h
+++ PixelProject/DebugStopWatch.h
@@ -7,46 +7,50 @@
 class DebugStopWatch
 {
 public:
-		char out_string[50] = "\0";
+   char out_string[50] = "\0";
 
-		void AddTimer(const char* timer_name, const bool enabled) {
-				stop_watch_enabled_[timer_name] = enabled;
-				stop_watch_time_[timer_name] = SDL_GetTicks();
-				keys_.push_back(timer_name);
-		}
+   void AddTimer(const char* timer_name, const bool enabled) {
+      stop_watch_enabled_[timer_name] = enabled;
+      stop_watch_time_[timer_name] = SDL_GetTicks();
+      keys_.push_back(timer_name);
+   }
 
-		void SetTimerState(const char* timer_name, const bool enabled) {
-				stop_watch_enabled_[timer_name] = enabled;
-		}
+   void SetTimerState(const char* timer_name, const bool enabled) {
+      stop_watch_enabled_[timer_name] = enabled;
+   }
 
-		std::string GetData() {
-				std::string result;
-				// Early abort since this can fire before all times are filled.
-				// TODO Fix? Do I care? eh
-				if (keys_.size() != times_.size())
-						return result;
-				for (short i = 0; i < keys_.size(); i++)
-				{
-						if (stop_watch_enabled_[keys_[i]]) {
-								snprintf(out_string, 50, "%i\tms\t%s\n", times_[keys_[i]], keys_[i]);
-								result += out_string;
-						}
-				}
-				return result;
-		}
+   std::string GetData() {
+      std::string result;
+      // Early abort since this can fire before all times are filled.
+      // TODO Fix? Do I care? eh
+      if (keys_.size() != times_.size())
+         return result;
+      for (short i = 0; i < keys_.size(); i++)
+      {
+         if (stop_watch_enabled_[keys_[i]]) {
+            snprintf(out_string, 50, "%0.1f\tms\t%s\n", times_[keys_[i]], keys_[i]);
+            result += out_string;
+         }
+      }
+      return result;
+   }
 
-		void StoreTime(const char* timer_name) {
-				times_[timer_name] = (SDL_GetTicks() - stop_watch_time_[timer_name]);
-		};
-		void UpdateTime(const char* timer_name) {
-				stop_watch_time_[timer_name] = SDL_GetTicks();
-		}
-		DebugStopWatch() {};
-		~DebugStopWatch() {};
+   void StoreTime(const char* timer_name) {
+      times_[timer_name] = (SDL_GetTicks() - stop_watch_time_[timer_name]);
+   };
+   void StoreTime(const char* timer_name, const float time)
+   {
+      times_[timer_name] = time;
+   }
+   void UpdateTime(const char* timer_name) {
+      stop_watch_time_[timer_name] = SDL_GetTicks();
+   }
+   DebugStopWatch() {};
+   ~DebugStopWatch() {};
 
 private:
-		std::vector<const char*> keys_;
-		std::unordered_map<const char*, Uint32> times_;
-		std::unordered_map<const char*, bool> stop_watch_enabled_;
-		std::unordered_map<const char*, Uint32> stop_watch_time_;
+   std::vector<const char*> keys_;
+   std::unordered_map<const char*, float> times_;
+   std::unordered_map<const char*, bool> stop_watch_enabled_;
+   std::unordered_map<const char*, Uint32> stop_watch_time_;
 };
