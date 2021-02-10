9bf5d9864d4c875f97f1d60560822503d67e127b
James Kellie <Jpk_GameID@hotmail.com>
2021-02-10 21:00:17 +1100

    Update GameSettings.h

--- PixelProject/GameSettings.h
+++ PixelProject/GameSettings.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <gl/GLEW.h>
+
 #include "SDL_FontCache/SDL_FontCache.h"
 #include "Vec2.h"
 #include "DebugStopWatch.h"
@@ -7,6 +9,7 @@
 
 struct GameSettings
 {
+   GLint default_shader;
    FC_Font* font = nullptr;
 
    DebugStopWatch* stop_watch = nullptr;
@@ -16,6 +19,7 @@ struct GameSettings
 
    /* Game Settings that will be global */
    IVec2 screen_size = IVec2(1280, 720);
+   float aspect_ratio = screen_size.x / screen_size.y;
 
    float target_frames_per_second = 60.0f;
    float calculated_frame_delay = 1000.0f / target_frames_per_second;
@@ -24,9 +28,11 @@ struct GameSettings
    {
       screen_size.x = config.screen_x;
       screen_size.y = config.screen_y;
+      // Set aspect ratio for future use
+      aspect_ratio = screen_size.x / static_cast<float>(screen_size.y);
+      // Target updates from config (FPS)
       target_frames_per_second = config.target_frames_per_second;
 
-      // Apply changes
       calculated_frame_delay = 1000.0f / target_frames_per_second;
    };
 };
