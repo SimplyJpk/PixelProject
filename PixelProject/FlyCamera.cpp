7bee71a6d5b287fec5fc761f045e480414277d8b
James Kellie <Jpk_GameID@hotmail.com>
2021-02-18 16:14:59 +1100

    Cleaned up Input changes
    - New IsMovement method which should simplify hooked classes looking for inputs.
    - Cleaned up FlyCam to use controls better suited to environment, still needs settings.

--- PixelProject/FlyCamera.cpp
+++ PixelProject/FlyCamera.cpp
@@ -7,7 +7,7 @@ FlyCamera::FlyCamera()
 // Updates camera position and rotation
 void FlyCamera::Update(const float delta)
 {
-   if (InputManager::Instance()->GetKeyButton(KeyCode::Space))
+   if (InputManager::Instance()->IsMovementKeysDown())
    {
       MoveCam(delta);
    }
@@ -23,20 +23,6 @@ void FlyCamera::MoveCam(const float delta)
 
   auto* im = InputManager::Instance();
 
-	if (im->GetKeyButton(KeyCode::LShift))
-		move_speed_ = default_speed_ * shift_multi_;
-	else
-		move_speed_ = default_speed_;
-
-	// Forward Back
-	if (im->GetKeyButton(KeyCode::W))
-	{
-		currentPos -= forward* delta* move_speed_;
-	}
-	else if (im->GetKeyButton(KeyCode::S))
-	{
-		currentPos += forward* delta * move_speed_;
-	}
 	// Left Right
 	if (im->GetKeyButton(KeyCode::A))
 	{
@@ -47,13 +33,22 @@ void FlyCamera::MoveCam(const float delta)
 		currentPos += left* delta * move_speed_;
 	}
 	// Up Down
-	if (im->GetKeyButton(KeyCode::Q))
+	if (im->GetKeyButton(KeyCode::S))
 	{
 		currentPos -= up* delta* move_speed_;
 	}
-	else if (im->GetKeyButton(KeyCode::E))
+	else if (im->GetKeyButton(KeyCode::W))
 	{
 		currentPos += up* delta * move_speed_;
+	}
+	 	// Forward Back
+	if (im->GetKeyButton(KeyCode::Q))
+	{
+		currentPos -= forward* delta* move_speed_;
+	}
+	else if (im->GetKeyButton(KeyCode::E))
+	{
+		currentPos += forward* delta * move_speed_;
 	}
 
 	SetPosition(currentPos);
