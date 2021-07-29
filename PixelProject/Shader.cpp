5dfca809ed301f98efe8464ade4823c20608307f
James Kellie <Jpk_GameID@hotmail.com>
2021-07-29 15:26:53 +1000

    Give Shaders a variable to hold a name
    Improves debugging if provided, otherwise default "_NO_NAME_" is used. ProgramID is passed through logging in the result of warnings which might be enough for general debugging.

--- PixelProject/Shader.cpp
+++ PixelProject/Shader.cpp
@@ -5,6 +5,12 @@ Shader::Shader(const GLuint program_id)
    program_ = program_id;
 }
 
+Shader::Shader(const GLuint program_id, const std::string& name)
+{
+   program_ = program_id;
+   name_ = name;
+}
+
 void Shader::UseProgram() const
 {
    glUseProgram(program_);
@@ -15,23 +21,23 @@ GLuint Shader::GetProgramID() const
    return program_;
 }
 
-GLuint Shader::GetUniformLocation(const std::string& name)
+GLuint Shader::GetUniformLocation(const std::string& uniform_name)
 {
    // Check if in map already
    std::map<std::string, GLuint>::iterator i;
-   if ((i = uniform_locations_.find(name)) != uniform_locations_.end())
+   if ((i = uniform_locations_.find(uniform_name)) != uniform_locations_.end())
    {
       return i->second;
    }
    // Check if exists in shader
-   GLuint pos = glGetUniformLocation(program_, name.c_str());
+   GLuint pos = glGetUniformLocation(program_, uniform_name.c_str());
    if (pos != static_cast<GLuint>(-1))
    {
-      uniform_locations_.insert({ name, pos });
+      uniform_locations_.insert({ uniform_name, pos });
       return pos;
    }
    // Complain that it doesn't exist
    //TODO Throw? What is best to handle this?
-   printf("Invalid Uniform: '%s' does not exist.\n", name.c_str());
+   printf("Program [%i '%s']: '%s' does not exist.\n", program_, name_.c_str(), uniform_name.c_str());
    return -1;
 }
--- PixelProject/Shader.h
+++ PixelProject/Shader.h
@@ -12,12 +12,14 @@ class Shader
 {
 public:
    Shader(GLuint program_id);
+   Shader(GLuint program_id, const std::string& name);
 
    void UseProgram() const;
 
    GLuint GetProgramID() const;
-   GLuint GetUniformLocation(const std::string& name);
+   GLuint GetUniformLocation(const std::string& uniform_name);
 private:
    GLuint program_;
+   std::string name_ = "_NO_NAME_";
    std::map<std::string, GLuint> uniform_locations_;
 };
