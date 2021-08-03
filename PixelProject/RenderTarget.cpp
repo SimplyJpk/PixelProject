13e8438bb200d7ac4d17887cc5a60a2ede476fb5
James Kellie <Jpk_GameID@hotmail.com>
2021-08-03 15:49:42 +1000

    Edit to work for bloom
    Still needs work, can be tidied up significantly.

--- PixelProject/RenderTarget.cpp
+++ PixelProject/RenderTarget.cpp
@@ -1,7 +1,5 @@
 #include "RenderTarget.h"
 
-#include <gl/GLEW.h>
-
 // Creates a renderTarget using width and height given
 RenderTarget::RenderTarget(const unsigned int width, const unsigned int height)
 {
@@ -27,7 +25,8 @@ void RenderTarget::BindRenderTarget(bool clear) const
 	 glViewport(0, 0, static_cast<int>(buffer_size_.x), static_cast<int>(buffer_size_.y));
 
 	 // Clear screen/target
-	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	 if (clear)
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 // Draws the quad onto the screen at position x and y using shader provided
@@ -36,26 +35,82 @@ void RenderTarget::DrawTargetQuad(const int x, const int y, const bool clear) co
 	 // Make sure we're not drawing back into our buffer
 	 glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	 glViewport(x, y, static_cast<int>(buffer_size_.x), static_cast<int>(buffer_size_.y));
-	 // Clear the depth buffer
-	 if (clear)
-			glClear(GL_DEPTH_BUFFER_BIT);
 
-	 // Use a different shader to the RenderTarget Shader
-	 post_shader_->UseProgram();
+	 //x // Use a different shader to the RenderTarget Shader
+	 //x post_shader_->UseProgram();
+	 //x
+	 //x // Set active texture of FBO
+	 //x glActiveTexture(GL_TEXTURE0);
+	 //x glBindTexture(GL_TEXTURE_2D, fbo_texture_[0]);
+	 //x
+	 //x // RenderTarget Shader
+	 //x int loc = post_shader_->GetUniformLocation("target");
+	 //x glUniform1i(loc, 0);
+	 //x // Set Kernal
+	 //x loc = post_shader_->GetUniformLocation("KernalState");
+	 //x glUniform1i(loc, kernal_state);
+	 //x
+	 //x glBindVertexArray(vao_);
+	 //x glDrawArrays(GL_TRIANGLES, 0, 6);
+	 //x
+	 //x glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	 // Set active texture of FBO
+	 // 2. blur bright fragments with two-pass Gaussian Blur 
+	 // --------------------------------------------------
+	 bool horizontal = true, first_iteration = true;
+	 unsigned int amount = 10;
+	 Shader& blurShader = ShaderManager::Instance().GetShader("blur");
+	 blurShader.UseProgram();
 	 glActiveTexture(GL_TEXTURE0);
-	 glBindTexture(GL_TEXTURE_2D, fbo_texture_);
+	 for (unsigned int i = 0; i < amount; i++)
+	 {
+			glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
+			glUniform1i(blurShader.GetUniformLocation("horizontal"), horizontal);
+			glBindTexture(GL_TEXTURE_2D, first_iteration ? fbo_texture_[1] : pingpongColorbuffers[!horizontal]);  // bind texture of other framebuffer (or scene if first iteration)
 
-	 // RenderTarget Shader
-	 int loc = post_shader_->GetUniformLocation("target");
-	 glUniform1i(loc, 0);
-	 // Set Kernal
-	 loc = post_shader_->GetUniformLocation("KernalState");
-	 glUniform1i(loc, kernal_state);
+			glBindVertexArray(vao_);
+			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+			glBindVertexArray(0);
+
+			horizontal = !horizontal;
+			if (first_iteration)
+				 first_iteration = false;
+	 }
+	 glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	 // Uncomment to view Blur Bloom
+	 // glBindTexture(GL_TEXTURE_2D, fbo_texture_[1]);
+	 // glBindVertexArray(vao_);
+	 // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	 // glBindVertexArray(0);
+
+	 // 3. now render floating point color buffer to 2D quad and tonemap HDR colors to default framebuffer's (clamped) color range
+	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	 Shader& bloomShader = ShaderManager::Instance().GetShader("bloom");
+	 bloomShader.UseProgram();
+
+	 glActiveTexture(GL_TEXTURE0);
+	 glBindTexture(GL_TEXTURE_2D, fbo_texture_[0]);
+	 glActiveTexture(GL_TEXTURE1);
+	 glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[!horizontal]);
+	 // Bloom or not
+	 glUniform1i(bloomShader.GetUniformLocation("bloom"), true);
+
+	 // DEBUG for drawing bloom
+	 static int status = 0;
+	 if (InputManager::Instance()->GetKeyDown(KeyCode::_9))
+	 {
+	    status += 1;
+      if (status >= 3)
+         status -= 3;
+	 }
+	 glUniform1i(bloomShader.GetUniformLocation("state"), status);
+	 // DEBUG for drawing bloom
 
 	 glBindVertexArray(vao_);
-	 glDrawArrays(GL_TRIANGLES, 0, 6);
+	 glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	 glBindVertexArray(0);
 }
 
 void RenderTarget::SetupBuffer(unsigned int width, unsigned int height)
@@ -65,63 +120,81 @@ void RenderTarget::SetupBuffer(unsigned int width, unsigned int height)
 	 glBindFramebuffer(GL_FRAMEBUFFER, fbo_);
 
 	 // Create Texture and bind it
-	 glGenTextures(1, &fbo_texture_);
-	 glBindTexture(GL_TEXTURE_2D, fbo_texture_);
-	 // Texture format
-	 glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	 glGenTextures(2, fbo_texture_);
 
-	 // Attach it to framebuffer as first colour attachment
-	 glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fbo_texture_, 0);
+	 for (unsigned int i = 0; i < 2; i++)
+	 {
+			glBindTexture(GL_TEXTURE_2D, fbo_texture_[i]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			// attach texture to framebuffer
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, fbo_texture_[i], 0);
+	 }
 
 	 // Setup and bind 24bit depth buffer
 	 glGenRenderbuffers(1, &fbo_depth_);
 	 glBindRenderbuffer(GL_RENDERBUFFER, fbo_depth_);
-	 glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, width, height);
+	 glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
 
 	 // Bind FBO while still bound
 	 glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, fbo_depth_);
 
-	 GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0 };
-	 glDrawBuffers(1, drawBuffers);
+	 unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+	 glDrawBuffers(2, attachments);
 
 	 GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	 if (status != GL_FRAMEBUFFER_COMPLETE)
 			printf("Framebuffer Error!\n");
 
 	 glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	 // ping-pong-framebuffer for blurring
+	 glGenFramebuffers(2, pingpongFBO);
+	 glGenTextures(2, pingpongColorbuffers);
+	 for (unsigned int i = 0; i < 2; i++)
+	 {
+			glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
+			glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
+			// also check if framebuffers are complete (no need for depth buffer)
+			if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+				 printf("pingpong Framebuffer Error!\n");
+	 }
+
 }
 
 void RenderTarget::SetupVertex()
 {
-	 half_texel_ = 1.0f / glm::vec2(buffer_size_.x, buffer_size_.y) * 0.5f;
-
-	 // Setup a square
 	 float vertexData[] = {
-		 -1, -1, 0, 1,	(half_texel_.x),			(half_texel_.y),
-			1, 1, 0, 1,	(1 - half_texel_.x),		(1 - half_texel_.y),
-		 -1, 1, 0, 1,	(half_texel_.x),			(1 - half_texel_.y),
-
-		 -1, -1, 0, 1,	(half_texel_.x),			(half_texel_.y),
-		 1, -1, 0, 1,	(1 - half_texel_.x),		(half_texel_.y),
-		 1, 1, 0, 1,		(1 - half_texel_.x),		(1 - half_texel_.y) };
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+	 };
 
 	 glGenVertexArrays(1, &vao_);
 	 glBindVertexArray(vao_);
 
 	 glGenBuffers(1, &vbo_);
 	 glBindBuffer(GL_ARRAY_BUFFER, vbo_);
-	 glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 6, vertexData, GL_STATIC_DRAW);
+	 glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), &vertexData, GL_STATIC_DRAW);
 
 	 // Position
 	 glEnableVertexAttribArray(0);
-	 glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+	 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 
 	 // TexCoord
 	 glEnableVertexAttribArray(1);
-	 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, static_cast<char*>(0) + 16);
+	 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 
 	 glBindVertexArray(0);
-	 glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
